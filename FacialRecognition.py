c9c3b71020f0c5977578e80c92fdc69e49481f74
abiralmahdi <abiralmahdi@gmail.com>
2024-09-22 17:07:42 +0600

    drew rectangles around thedetected faces in the picture

--- FacialRecognition.py
+++ FacialRecognition.py
@@ -33,7 +33,13 @@ def cropOut(image_path, userImg):
     image = cv2.imread(image_path)
     imgArray = [] 
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
 
+    for (x, y, w, h) in faces:
+        cv2.rectangle(image, (x, y), (x + w, y + h), (255, 0, 0), 2)      
+    cv2.imshow('Image Window', image)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
 
     return None
 

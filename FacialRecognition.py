e9f64bd48886ebe7e8335728bf4209330d14eb0a
abiralmahdi <abiralmahdi@gmail.com>
2024-09-22 16:56:17 +0600

    Comments

--- FacialRecognition.py
+++ FacialRecognition.py
@@ -4,6 +4,10 @@ import face_recognition # pretrained model for facial features recognition
 # Load the pre-trained Haar Cascade classifier for face detection
 face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 
+# Arrays which contain some random group images and some random user's images
+arrUsers = ["abir.jpg","rdj.jpg", "rdj2.jpg", "biden.jpg"]
+arrGroupImgs = ["group.webp", "rdj4.jpg", "rdj3.jpg","abirGroup.jpg"]
+
 def recognition(imgArr, userImg):
     known_image = face_recognition.load_image_file(userImg) # Loading the image of the user
     known_encoding = face_recognition.face_encodings(known_image)[0] # Encoding of the user's image

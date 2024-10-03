2ec9d6f76fff27c3aba516b1a39e5cf67f08d89e
EzazAsif <asifrafid8399@gmail.com>
2024-10-03 15:14:47 +0600

    fixed the API app and annded them back

--- FR/FR/urls.py
+++ FR/FR/urls.py
@@ -1,8 +1,7 @@
 from django.contrib import admin
 from django.urls import path
 from django.urls import path, include
-from rest_framework.routers import DefaultRouter
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenVerifyView
+
 
 
 
@@ -12,6 +11,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     
     path('accounts/', include('accounts.urls')), 
+    path('api/', include('API.urls')), 
 
 ]
 
